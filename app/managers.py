5ad6adf292a79fc8c61b86ea370f653808ce564c
Білоус Антон <a.bilous2@avrora.ua>
2025-10-17 10:27:00 +0300

    s

--- app/managers.py
+++ app/managers.py
@@ -12,19 +12,17 @@ class ActorManager:
         print(f"✅ Connected to {self.db_name}")
 
         self.cursor.execute(
-            f"""
-            CREATE TABLE IF NOT EXISTS {self.table_name} (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                first_name TEXT NOT NULL,
-                last_name TEXT NOT NULL
-            )
-            """
+            f"CREATE TABLE IF NOT EXISTS {self.table_name} ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            "first_name TEXT NOT NULL, "
+            "last_name TEXT NOT NULL)"
         )
         self.connect.commit()
 
     def create(self, first_name: str, last_name: str) -> Actor:
         self.cursor.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name}"
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name),
         )
         self.connect.commit()
@@ -32,13 +30,22 @@ class ActorManager:
         return Actor(id=actor_id, first_name=first_name, last_name=last_name)
 
     def all(self) -> list[Actor]:
-        self.cursor.execute(f"SELECT id, first_name, last_name FROM {self.table_name}")
+        self.cursor.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name}"
+        )
         rows = self.cursor.fetchall()
-        return [Actor(id=row[0], first_name=row[1], last_name=row[2]) for row in rows]
+        return [
+            Actor(
+                id=row[0],
+                first_name=row[1],
+                last_name=row[2]
+            ) for row in rows
+        ]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self.cursor.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} SET"
+            " first_name=?, last_name=? WHERE id=?",
             (new_first_name, new_last_name, pk),
         )
         self.connect.commit()
