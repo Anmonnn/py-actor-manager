201ca58e3f082f21801d794c8d2f207f8df66030
Білоус Антон <a.bilous2@avrora.ua>
2025-10-17 09:56:31 +0300

    save

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,27 @@
 import sqlite3
-
 from app.models import Actor
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self.db_name = db_name
+        self.table_name = table_name
+
+        self.connect = sqlite3.connect(self.db_name)
+        self.cursor = self.connect.cursor()
+        print(f"✅ Connected to {self.db_name}")
+
+    def create(self, first_name: str, last_name: str) -> None:
+        Actor.objects.create(first_name=first_name, last_name=last_name)
+
+    def all(self) -> list:
+        return Actor.objects.all()
+
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        Actor.objects.filter(id=pk).update(
+            first_name=new_first_name, last_name=new_last_name
+        )
+
+    def delete(self, pk) -> None:
+        user = Actor.objects.get(id=pk)
+        user.delete()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
+from django.db import models
 
-
+@dataclass
+class Actor(models.Model):
+    first_name: str
+    last_name: str
 # add dataclass here
